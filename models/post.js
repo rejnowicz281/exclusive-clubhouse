24349e91648aa4d617c09fd9d1a22051efcebea1
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2023-08-03 12:52:35 +0200

    Create virtuals for post's formatted dates

--- models/post.js
+++ models/post.js
@@ -20,4 +20,24 @@ postSchema.virtual("url").get(function () {
     return "/posts/" + this._id;
 });
 
+// Virtual for post's formatted createdAt date
+postSchema.virtual("createdAt_formatted").get(function () {
+    return this.createdAt.toLocaleDateString("en-US", {
+        weekday: "long",
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+    });
+});
+
+// Virtual for post's formatted updatedAt date
+postSchema.virtual("updatedAt_formatted").get(function () {
+    return this.updatedAt.toLocaleDateString("en-US", {
+        weekday: "long",
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+    });
+});
+
 export default mongoose.model("Post", postSchema);
